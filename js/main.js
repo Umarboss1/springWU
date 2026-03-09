852ecc87042483ed5d4c99fd1e9a236412e36521
Umarboss1 <muhammad.abbas@elev.ga.ntig.se>
2026-03-09 14:33:07 +0100

    FIxade mobilläge

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobileMenu");
+const fadeLayer = document.getElementById("fadeLayer");
 
-hamburger.addEventListener("click", () =>{
+
+hamburger.addEventListener("click", showMenu);
+fadeLayer.addEventListener("click", showMenu);
+
+function showMenu(){
     mobileMenu.classList.toggle("active");
-});
+    fadeLayer.classList.toggle("visible");
+}
